45ea6df79e5723c5743f1dd73abc72151897abc6
avinash <khadsanavinash01@gmail.com>
2018-10-02 18:31:26 +0530

    Hibernate configuration done

--- src/main/java/com/brocker/model/User.java
+++ src/main/java/com/brocker/model/User.java
@@ -1,5 +1,20 @@
 package com.brocker.model;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+/**
+ * 
+ * @author Avinash
+ *
+ */
+@Entity
+@Table(name = "BR_USER")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 }
